f1c21e45cac095c90c35888fb6c957ba726b9956
Staerman guillaume <>
2021-04-06 11:36:41 +0200

    no message

--- setup.py
+++ setup.py
@@ -15,19 +15,21 @@ def read(filename):
     return open(os.path.join(prjdir, filename)).read()
 
 
+
+
 extra_link_args = []
 libraries = []
 library_dirs = []
 include_dirs = []
 exec(open('version.py').read())
 setup(
-    name='fif',
+    name='python-fif',
     version=__version__,
     author='Guillaume Staerman',
     author_email='guillaume.staerman@telecom-paris.fr',
     cmdclass={'build_ext': build_ext},
     ext_modules=[Extension("fif",
-                 sources=["_fif.pyx", "fif.cxx"],
+                 sources=["_fif.pyx", "fif.cxx", "__fif.pxd"],
                  include_dirs=[numpy.get_include()],
                  extra_compile_args=['-std=c++11', '-Wcpp'],
                  language="c++")],
@@ -37,8 +39,7 @@ setup(
     license='License.txt',
     include_package_data=True,
     description='Functional Isolation Forest',
-    long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    url='https://github.com/GuillaumeStaermanML/FIF',
+    url='https://github.com/GuillaumeStaermanML/FIF/archive/refs/tags/1.0.0.tar.gz',
     install_requires=["numpy", "cython"],
 )
