2f40d0c2d616d6c3abeeaaf28634aa7ddcd4a956
Staerman guillaume <>
2021-04-06 11:46:43 +0200

    new Cython/C++ code!

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author_email='guillaume.staerman@telecom-paris.fr',
     cmdclass={'build_ext': build_ext},
     ext_modules=[Extension("fif",
-                 sources=["_fif.pyx", "fif.cxx", "__fif.pxd"],
+                 sources=["_fif.pyx", "fif.cxx"],
                  include_dirs=[numpy.get_include()],
                  extra_compile_args=['-std=c++11', '-Wcpp'],
                  language="c++")],
@@ -40,6 +40,6 @@ setup(
     include_package_data=True,
     description='Functional Isolation Forest',
     long_description_content_type='text/markdown',
-    url='https://github.com/GuillaumeStaermanML/FIF/archive/refs/tags/1.0.0.tar.gz',
+    url='https://github.com/GuillaumeStaermanML/FIF',
     install_requires=["numpy", "cython"],
 )
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """Functional Isolation Forest version"""
 
-version_tag = (1, 0, 0)
+version_tag = (1, 0, 1)
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
